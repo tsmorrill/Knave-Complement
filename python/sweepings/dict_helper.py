1b03a6b24ea8b9f478ba4110babb8f46f83693ac
Tamsyn Morrill <morrill.math@gmail.com>
2023-09-07 11:08:18 -0400

    Delete dict_helper.py

--- python/sweepings/dict_helper.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-
-words = tuple("W" + str(i) for i in (1,) + tuple(range(3, 8)))
-strings = (14*" " + f"({x}, {y}): W," for x, y in product(words, words))
-
-for x in strings:
-    print(x)
